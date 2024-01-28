910e1fdee840bc979c0463a5e589fcfba1f91121
TilavovD <tilavov.dostonbek@gmail.com>
2024-01-28 17:56:31 +0500

    feat: added login view to authenticate user

--- account/views.py
+++ account/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        cd = form.cleaned_data
+        user = authenticate(request, username=cd['username'], password=cd['password'])
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                HttpResponse("Auth was successful")
+            else:
+                HttpResponse("Disabled user")
+        else:
+            HttpResponse("User not found")
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
